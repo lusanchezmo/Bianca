d7c9a9e7095614351fb740b14f16a1efa0a0d78a
Luis <lusanchezmo@unal.edu.co>
2023-07-13 11:48:18 -0500

    Se crea el backend, se hace la conexion con la base de datos, y se hace la consulta de los apartamentos para mostrarlos en el frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Aptos from './components/Aptos';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Aptos />
   </React.StrictMode>
 );
 
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json())
+
+app.use(
+    cors({
+      origin: "http://localhost:3000", // allow requests only from this origin
+    })
+  );
+
+app.get('/',(req,res) => {
+
+    var conn = require('./db.js');  // !!INCLUIR SIEMPRE!!  se incluye archivo db.js
+    var con = conn.con();           // se llama la función createConection(), se almacena en con, esta es una variable para realizar la conección
+
+    con.connect(function (err) {    // se abre la conexion con la db
+        if (err) throw err;         // validacion de apertura
+        con.query("select apto from ingruma2;", function (err, result, fields) { // se envía la petición a DB
+
+            if (err) throw err;  // valida peticion enviada corrrectamente
+            res.send(JSON.stringify(result));    // se imprime en pantalla el resultado de la consulta
+
+        });
+    });
+});
+
+app.listen(5000);
+console.log('server on port 5000');
